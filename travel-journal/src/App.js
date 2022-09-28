39df2f017f1b910c669bdbd68d85056a69f8036b
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2022-09-28 10:25:10 -0700

    Add tripcard(), clean up data file

--- travel-journal/src/App.js
+++ travel-journal/src/App.js
@@ -1,13 +1,19 @@
 import logo from './logo.svg';
+import TravelData from "./data"
+import TripCard from './components/tripcard';
 import Header from "./components/header"
 
 
 function App() {
+  // get data
+  
+
   return (
     <div>
     <Header />
+    <TripCard />
     <div className="App">
-     Travel app goes here
+     Travel app here
     </div>
     </div>
   );
--- travel-journal/src/data.js
+++ travel-journal/src/data.js
@@ -1,8 +1,8 @@
 export default [
     {
         id: 1,
-        title: "Lava dripping into ocean",
-        location:"Volcanos National Park, Big Island, Hawaii, USA",
+        title: "Volcanos National Park",
+        location:"Big Island, Hawaii, USA",
         googleMapsUrl:"https://goo.gl/maps/dPk4XiWD7roXFjhx8",
         visitDate:"June 2010",
         description:"Unique destination to spot live lava flows, lava tubes & glowing craters is popular with tourists.",
@@ -13,8 +13,8 @@ export default [
 
     {
         id: 2,
-        title: "Panda sitting against a tree",
-        location: "Panda breeding center, Chengdu, China",
+        title: "Panda Breeding center",
+        location: "Chengdu, China",
         googleMapsUrl:"https://goo.gl/maps/FmjbC4LwUwyDLLGp6",
         visitDate:"July 2008",
         description:"Giant Panda conservation facility allowing visitors to observe pandas & other endangered animals.",
