62c8e5abcb0da6f309d84a869af6be6e2f545fcf
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2022-09-29 15:14:43 -0700

    add data

--- travel-journal/src/data.js
+++ travel-journal/src/data.js
@@ -16,10 +16,21 @@ export default [
         title: "Panda Breeding center",
         location: "Chengdu, China",
         googleMapsUrl:"https://goo.gl/maps/FmjbC4LwUwyDLLGp6",
-        visitDate:"July 2008",
+        visitDate:"July 2006",
         description:"Giant Panda conservation facility allowing visitors to observe pandas & other endangered animals.",
         imageURL:"https://images.unsplash.com/photo-1526716173434-a1b560f2065d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
         photographerCredits:"https://unsplash.com/photos/NsNRu6dfRds"
+    }
+    ,
+    {
+        id: 3,
+        title: "Eiffel Tower",
+        location: "Paris, France",
+        googleMapsUrl:"https://goo.gl/maps/2Fey2HTc1FdVzm5g6",
+        visitDate:"July 2008",
+        description:"Gustave Eiffel's iconic, wrought-iron 1889 tower, with steps and elevators to observation decks.",
+        imageURL:"https://images.unsplash.com/photo-1549144511-f099e773c147?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+        photographerCredits:"https://unsplash.com/photos/WSvth_lwCi0"
     }
 
 
