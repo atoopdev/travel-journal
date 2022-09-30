52b1e57785683d9ef617be5851998ce2eb9bdd5a
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2022-09-30 10:31:29 -0700

    Update data.js

--- travel-journal/src/data.js
+++ travel-journal/src/data.js
@@ -6,8 +6,8 @@ export default [
         googleMapsUrl:"https://goo.gl/maps/dPk4XiWD7roXFjhx8",
         visitDate:"June 2010",
         description:"Unique destination to spot live lava flows, lava tubes & glowing craters is popular with tourists.",
-        photographerCredits:"https://unsplash.com/photos/Aduh0KXCI1w",
-        imageURL:"https://images.unsplash.com/photo-1518457607834-6e8d80c183c5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2748&q=80"
+        photographerCredits:"https://unsplash.com/photos/xfSdarS1wus",
+        imageURL:"https://images.unsplash.com/photo-1509780764860-beca5b971c4b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
 
     },
 
