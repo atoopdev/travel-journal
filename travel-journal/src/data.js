e5b6066dfd300efb99e14cd4e359646855ac50fd
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2022-09-30 10:00:42 -0700

    Update data.js

--- travel-journal/src/data.js
+++ travel-journal/src/data.js
@@ -25,12 +25,22 @@ export default [
     {
         id: 3,
         title: "Eiffel Tower",
-        location: "Paris, France",
+        location: "Jinshanling Great Wall, China",
         googleMapsUrl:"https://goo.gl/maps/2Fey2HTc1FdVzm5g6",
         visitDate:"July 2008",
         description:"Gustave Eiffel's iconic, wrought-iron 1889 tower, with steps and elevators to observation decks.",
-        imageURL:"https://images.unsplash.com/photo-1549144511-f099e773c147?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+        imageURL:"https://images.unsplash.com/flagged/photo-1552553030-837c9c2b0fac?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
         photographerCredits:"https://unsplash.com/photos/WSvth_lwCi0"
+    },
+    {
+        id: 4,
+        title: "Great Wall",
+        location: "Paris, France",
+        googleMapsUrl:"https://goo.gl/maps/cr6Si57ueaAXMX3cA",
+        visitDate:"July 2010",
+        description:"Natural area featuring an ample section of the Great Wall of China & striking views.",
+        imageURL:"https://images.unsplash.com/photo-1549144511-f099e773c147?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+        photographerCredits:"https://unsplash.com/photos/2s1chnvuMQ4"
     }
 
 
